b078458d8e22194dc4f0555355ee96d65fc34e7f
champion2030 <yar-resh@yandex.ru>
2021-01-20 23:55:50 +0300

    Updated

--- src/components/MyProducts/MyProducts.jsx
+++ src/components/MyProducts/MyProducts.jsx
@@ -3,46 +3,51 @@ import s from "./MyProducts.module.css"
 import {NavLink} from "react-router-dom";
 import axios from "axios";
 
-let MyProducts = (props) => {
-    if (props.productsData.length === 0) {
+class MyProducts extends React.Component {
+
+    constructor(props){
+        super(props);
         axios.get("http://localhost:5000/main").then(response => {
                 props.setProducts(response.data)
             }
         )
     }
-    return <div className={s.products}>
-        {
-            props.productsData.map(u => <div key={u.id}>
-                <div className={s.productItem}>
-                    <div className={s.productAlign}>
-                        <div>
-                            <img src={u.photo} className={s.photo} alt={"Товар"}/>
-                        </div>
-                        <div>
-                            <NavLink to={"/product/" + u.id}>{u.title}</NavLink>
-                        </div>
-                        <div className={s.comments}>
-                            <NavLink to={"/product/" + u.id}>Комментарии</NavLink>
-                        </div>
-                        <div>
-                            <p>{u.price}</p>
+
+    render(){
+        return <div className={s.products}>
+            {
+                this.props.productsData.map(u => <div key={u.id}>
+                    <div className={s.productItem}>
+                        <div className={s.productAlign}>
                             <div>
-                                {
-                                    u.bought
-                                        ? <button onClick={() => {
-                                            props.buy(u.id)
-                                        }}>Cancel</button>
-                                        : <button onClick={() => {
-                                            props.cancel(u.id)
-                                        }}>Buy</button>
-                                }
+                                <img src={u.photo} className={s.photo} alt={"Товар"}/>
+                            </div>
+                            <div>
+                                <NavLink to={"/product/" + u.id}>{u.title}</NavLink>
+                            </div>
+                            <div className={s.comments}>
+                                <NavLink to={"/product/" + u.id}>Комментарии</NavLink>
+                            </div>
+                            <div>
+                                <p>{u.price}</p>
+                                <div>
+                                    {
+                                        u.bought
+                                            ? <button onClick={() => {
+                                                this.props.buy(u.id)
+                                            }}>Cancel</button>
+                                            : <button onClick={() => {
+                                                this.props.cancel(u.id)
+                                            }}>Buy</button>
+                                    }
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>)
-        }
-    </div>
+                </div>)
+            }
+        </div>
+    }
 
 }
 
